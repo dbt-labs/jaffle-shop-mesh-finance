8dd489900cc4647f9cac9d5eb92f112c37253564
Mark Wan <mark.wan@fishtownanalytics.com>
2023-12-05 21:46:58 +0000

    single commit

--- models/marts/products.sql
+++ models/marts/products.sql
@@ -2,7 +2,15 @@ with
 
 products as (
 
-    select * from {{ ref('jaffle_shop_mesh_platform', 'stg_products') }}
+    select
+        product_id,
+        product_name,
+        product_type,
+        product_description,
+        product_price,
+        is_food_item,
+        is_drink_item
+    from {{ ref('jaffle_shop_mesh_platform', 'stg_products') }}
 
 )
 
