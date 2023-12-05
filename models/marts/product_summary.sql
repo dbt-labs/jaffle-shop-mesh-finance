828c7258a73fb68f55233c96e5b53b5ebe2e34f5
Mark Wan <mark.wan@fishtownanalytics.com>
2023-12-05 22:25:35 +0000

    for apac webinar

---
+++ models/marts/product_summary.sql
@@ -0,0 +1,13 @@
+select
+    count(
+        distinct product_id
+    ) as number_of_products,
+    count(
+        case when
+            is_food_item then 1 end
+    ) as number_of_food_items,
+    count(
+        case when
+            is_drink_item then 1 end
+    ) as number_of_drink_items
+from {{ ref('products', v=2) }}
