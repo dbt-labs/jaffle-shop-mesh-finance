0ee924be9dfbcfe256a4cacb546ab0b30bf6b647
Jeremy Cohen <jeremy@dbtlabs.com>
2024-04-14 21:11:14 +0200

    Fix is_x_order calc

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -17,9 +17,9 @@ order_items_summary as (
     select
 
         order_id,
-        count(case when is_food_item then 1 else 0 end)
+        sum(case when is_food_item then 1 else 0 end)
             as count_food_items,
-        count(case when is_drink_item then 1 else 0 end)
+        sum(case when is_drink_item then 1 else 0 end)
             as count_drink_items,
 
         sum(supply_cost) as order_cost
@@ -35,15 +35,16 @@ compute_booleans as (
     select
 
         orders.*,
-        order_items_summary.order_cost,
-        order_items_summary.count_food_items > 0 as is_food_order,
-        order_items_summary.count_drink_items > 0 as is_drink_order
+        coalesce(order_items_summary.order_cost, 0) as order_cost,
+        coalesce(order_items_summary.count_food_items > 0, 0) as is_food_order,
+        coalesce(order_items_summary.count_drink_items > 0, 0) as is_drink_order
 
     from orders
 
     left join
         order_items_summary
         on orders.order_id = order_items_summary.order_id
+
 )
 
 select * from compute_booleans
