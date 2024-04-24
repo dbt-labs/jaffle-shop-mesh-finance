acc0581b4066321677f452da6e1beeb814b08015
Jeremy Cohen <jtcohen6@gmail.com>
2024-04-24 14:11:32 +0200

    Revert "Refactor products + add profit margin"

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -17,13 +17,12 @@ order_items_summary as (
     select
 
         order_id,
-        sum(case when is_food_item then 1 else 0 end)
+        count(case when is_food_item then 1 else 0 end)
             as count_food_items,
-        sum(case when is_drink_item then 1 else 0 end)
+        count(case when is_drink_item then 1 else 0 end)
             as count_drink_items,
 
-        sum(supply_cost) as order_cost,
-        sum(product_price - supply_cost) / sum(product_price) as profit_margin
+        sum(supply_cost) as order_cost
 
     from order_items_table
 
@@ -36,17 +35,15 @@ compute_booleans as (
     select
 
         orders.*,
-        coalesce(order_items_summary.order_cost, 0) as order_cost,
-        coalesce(order_items_summary.profit_margin, 0) as profit_margin,
-        coalesce(order_items_summary.count_food_items > 0, 0) as is_food_order,
-        coalesce(order_items_summary.count_drink_items > 0, 0) as is_drink_order
+        order_items_summary.order_cost,
+        order_items_summary.count_food_items > 0 as is_food_order,
+        order_items_summary.count_drink_items > 0 as is_drink_order
 
     from orders
 
     left join
         order_items_summary
         on orders.order_id = order_items_summary.order_id
-
 )
 
 select * from compute_booleans
--- models/marts/products.sql
+++ models/marts/products.sql
@@ -1,42 +1,9 @@
-with products as (
+with
+
+products as (
 
     select * from {{ ref('jaffle_shop_mesh_platform', 'stg_products') }}
 
-),
-
-supplies as (
-
-    select * from {{ ref('jaffle_shop_mesh_platform', 'stg_supplies') }}
-
-),
-
-order_supplies_summary as (
-
-    select
-        product_id,
-        boolor_agg(is_perishable_supply) as is_perishable,
-        sum(supply_cost) as supply_cost
-
-    from supplies
-
-    group by 1
-
-),
-
-joined as (
-
-    select
-
-        products.*,
-        order_supplies_summary.is_perishable,
-        order_supplies_summary.supply_cost,
-        (products.product_price - order_supplies_summary.supply_cost)
-        / products.product_price as profit_margin
-
-    from order_supplies_summary
-    inner join products
-        on order_supplies_summary.product_id = products.product_id
-
 )
 
-select * from joined
+select * from products
